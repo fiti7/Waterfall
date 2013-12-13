d0c335ae2d098a6fc8fe6e34ee4021606fe75e28
Etai Klein <etaiklein@gmail.com>
2013-12-13 10:39:53 -0800

    redo2

--- src/src/CopyFiles.java
+++ src/src/CopyFiles.java
@@ -80,42 +80,6 @@ public class CopyFiles implements Runnable{
 							//System.out.println("copying");
 							Thread.sleep(1000);
 						}
-<<<<<<< HEAD
-						
-									
-								
-							
-						
-=======
-						else{
-							for(int j = 0; j < desiredFiles.length; j ++){
-								for(int i = 0; i < mylist.length; i ++){
-									if(mylist[i].contains(desiredFiles[j])){
-										System.out.println(mylist[i]);
-										if (new File(mylist[i]).exists()){
-										FileUtils.copyFileToDirectory(new File(mylist[i]), new File(OUTPUT_FOLDER));
-										}
-										else{
-											String mystring = "";
-											for(int k=0; k<mylist.length; k++){
-												mystring+= (mylist[k] + ", ");
-											}
-											if (!mystring.equals(temp)){
-												FileUtils.copyDirectory(new File(SOURCE_FOLDER), new File(OUTPUT_FOLDER));
-												logger.log("copied " + mystring.substring(0,mystring.length()-2) + "\nfrom " + 
-														SOURCE_FOLDER + " to " + OUTPUT_FOLDER);
-											}
-											temp = mystring;
-											//give it a rest
-											//System.out.println("copying");
-											Thread.sleep(1000);
-										}
-										
-									}
-								}
-							}
-						}
->>>>>>> 44282dc1f5be392c379422759a9a0fa883d7bd82
 
 					}
 				 catch (IOException | InterruptedException | IllegalArgumentException e) {
