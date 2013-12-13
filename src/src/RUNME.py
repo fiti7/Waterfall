44282dc1f5be392c379422759a9a0fa883d7bd82
etaiklein <etaiklein@gmail.com>
2013-12-13 18:10:06 -0500

    prep for more data gathering

--- src/src/RUNME.py
+++ src/src/RUNME.py
@@ -1,19 +1,13 @@
+import subprocess
+import glob
 
 
-# open the 5 folders next to me.
-#run formatchart6.py for each.
+pythonpath = "C:/Python33/python.exe"
+    
+for filepath in glob.glob("./*/*.py"):
+    print(filepath)
+    subprocess.Popen([pythonpath, filepath])
 
-for dirname, dirnames, filenames in os.walk('.'):
-    # print path to all subdirectories first.
-    for subdirname in dirnames:
-        print os.path.join(dirname, subdirname)
 
-    # print path to all filenames.
-    for filename in filenames:
-        print os.path.join(dirname, filename)
 
-    # Advanced usage:
-    # editing the 'dirnames' list will stop os.walk() from recursing into there.
-    if '.git' in dirnames:
-        # don't go into any .git directories.
-        dirnames.remove('.git')
+    
