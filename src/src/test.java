6f68ca6fc04e416fad981509aead468bf7a0c947
etaiklein <etaiklein@gmail.com>
2013-12-22 15:29:54 -0500

    testing python exec

---
+++ src/src/test.java
@@ -0,0 +1,20 @@
+
+public class test{
+
+private static String cmd = “C:/Windows/System32/cmd.exe”;
+private static String py = “C:/python33/python.exe”
+private static String out = “.”
+
+public static void main(String[] args){
+
+try{
+Process p = Runtime.getRuntime().exec(cmd + “ /c ” + py + “ “ + out + “/formatchart.py”, null null);
+} catch (IOException e){
+e.printStackTrace();
+}
+
+
+}
+
+
+}
